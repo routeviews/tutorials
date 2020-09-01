fd01692cfa23b510ba983514287eab1520d8cc63
Teach <dteach@routeviews.org>
2020-09-01 15:56:45 -0700

    update examples to use routeviews-stream project

--- pybgpstream/example1.py
+++ pybgpstream/example1.py
@@ -5,18 +5,17 @@ from pybgpstream import BGPStream
 import time
 
 #Initialize BGPStream and set it's data interfaces options
-bs = BGPStream()
-bs.stream.set_data_interface('kafka')
-bs.stream.set_data_interface_option('kafka', 'brokers', 'stream.routeviews.org:9092')
-bs.stream.set_data_interface_option('kafka', 'topic', "^routeviews\.linx\..+\.bmp_raw")
-bs.set_live_mode()
+bs = BGPStream(
+        project='routeviews-stream',
+        filter="router amsix",
+        )
 
 #Print records yielded from bs.records() in a bgpreader-like format
 for rec in bs.records():
+    print(rec.project, rec.collector, rec.router)
     #make our date human readable
     rec_time = time.strftime('%y-%m-%d %H:%M:%S', time.localtime(rec.time))
     for elem in rec:
-        
         print(f"{elem.record_type}|{elem.type}|{rec_time}|{elem.router}|{elem.router_ip}"
               f"|{elem.peer_asn}|{elem.peer_address}|{elem._maybe_field('prefix')}"
               f"|{elem._maybe_field('next-hop')}|{elem._maybe_field('as-path')}")
--- pybgpstream/example2.py
+++ pybgpstream/example2.py
@@ -10,10 +10,10 @@ import requests
 
 if __name__ == '__main__':
     #Initialize BGPStream and set it's data interfaces options
-    bs = BGPStream()
-    bs.stream.set_data_interface('kafka')
-    bs.stream.set_data_interface_option('kafka', 'brokers', 'stream.routeviews.org:9092')
-    bs.stream.set_data_interface_option('kafka', 'topic', "^routeviews\.linx\..+\.bmp_raw")
+    bs = BGPStream(
+            project="routeviews-stream",
+            filter="router amsix",
+            )
     bs.set_live_mode()
     print("starting stream...")
     while True:
